5f4facd9553bc353f6010cc74cc5d790f3b7c33c
Froot <mistergame177@gmail.com>
2021-10-31 00:02:41 +0300

    Готова версия 1.0

--- main.py
+++ main.py
@@ -13,6 +13,17 @@ command_dict = {
     '/start_learning': 5,
 }
 
+answer_dict = {
+
+    1:'Привет, я автоответчки Froot.\n В данный момент, мой хозяин занят, он ответит тебе позже.\n P.s: ЕСЛИ СРОЧНО НАБЕРИ МНЕ ',
+    2:'Привет, я автоответчки Froot.\n В данный момент, мой хозяин управляет ТС, он ответит тебе позже.\n P.s: ЕСЛИ СРОЧНО НАБЕРИ МНЕ ',
+    3:'Привет, я автоответчки Froot.\n В данный момент, мой хозяин зарабатывает бабосики, он ответит тебе позже.\n P.s: ЕСЛИ СРОЧНО НАБЕРИ МНЕ ',
+    4:'Привет, я автоответчки Froot.\n В данный момент, мой хозяин спит, он ответит тебе позже.\n P.s: ЕСЛИ СРОЧНО НАБЕРИ МНЕ ',
+    5:'Привет, я автоответчки Froot.\n В данный момент, мой хозяин разминает мозги, он ответит тебе позже.\n P.s: ЕСЛИ СРОЧНО НАБЕРИ МНЕ ',
+
+}
+
+
 status_counter = 0
 
 user_list = []
@@ -21,12 +32,17 @@ user_list = []
 @client.on(events.NewMessage)
 async def my_event_handler(event):
     global status_counter
-    if str(event.message.peer_id.channel_id).split()[0] == '1406135438':
-        if event.message.message in command_dict:
-            status_counter = command_dict[event.message.message]
+    if list(str(event.chat_id))[0] != '-' and str(event.from_id.user_id).split()[0] != '477643858':                  #Проверка что сообщение пришло от ползователя именно мне в чат
+        if status_counter != 0:
+            await event.reply(answer_dict[status_counter])
+    
+    try:
+        if str(event.message.peer_id.channel_id).split()[0] == '1406135438':
+            if event.message.message in command_dict:
+                status_counter = command_dict[event.message.message]
+    except:
+        pass
 
-    elif list(str(event.chat_id))[0] != '-' and str(event.from_id.user_id).split()[0] != '477643858':                  #Проверка что сообщение пришло от ползователя именно мне в чат
-        print(event)
     
     print(status_counter)
 
